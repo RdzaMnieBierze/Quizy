2c149d7a87ec3a8d3fdb259b045f5b763904468b
unknown <sommerantek@gmail.com>
2025-04-24 15:43:08 +0200

    Ignore

---
+++ MVVM/Model/Answer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizy.MVVM.Model
+{
+    public class Answer
+    {
+        string Content {  get; set; }
+        bool isCorrect;
+
+        public Answer(string content, bool isCorrect)
+        {
+            Content = content;
+            this.isCorrect = isCorrect;
+        }
+    }
+}
---
+++ MVVM/Model/Question.cs
@@ -0,0 +1,25 @@
+﻿using Quizy.MVVM.Viewmodel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizy.MVVM.Model
+{
+    class Question : ObservableObject
+    {
+        ObservableCollection<Answer>? Answers;
+        string Content;
+        Question(string[] ans, bool[] corr, string _content)
+        {
+            Content = _content;
+
+            for(int i = 0; i < ans.Length; i++)
+            {
+                Answers.Add(new Answer(ans[i], corr[i]));
+            }
+        }
+    }
+}
