8e7a3fd5fcfea11111d2ddae6432664503712630
unknown <sommerantek@gmail.com>
2025-04-24 13:39:40 +0200

    First

---
+++ MVVM/Viewmodel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Quizy.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Quizy.MVVM.Viewmodel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public RelayCommand exit => new RelayCommand(execute => ExitButton());
+        private void ExitButton()
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
