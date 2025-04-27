f0f7293299f4a3d63d19686aabb407845033c903
unknown <sommerantek@gmail.com>
2025-04-27 16:24:33 +0200

    createVM

--- MVVM/Viewmodel/CreateViewModel.cs
+++ MVVM/Viewmodel/CreateViewModel.cs
@@ -10,11 +10,11 @@ namespace Quizy.MVVM.Viewmodel
 {
     class CreateViewModel : ViewModelBase
     {
-        private Quiz CurrentQuiz;
-        public RelayCommand Add => new RelayCommand(_ => AddQuestion());
+        private Quiz? CurrentQuiz;
+        public RelayCommand Add => new RelayCommand(execute => AddQuestion());
 
 
-        private string _question;
+        private string? _question;
         public string Question
         {
             get => _question; 
@@ -34,11 +34,37 @@ namespace Quizy.MVVM.Viewmodel
                 OnPropertyChanged(nameof(Answers));
             }
         }
+        private bool[] _checked = new bool[4];
+        public bool[] Checked
+        {
+            get => _checked;
+            set
+            {
+                _checked = value;
+                OnPropertyChanged(nameof(Checked));
+            }
+        }
        
         private void AddQuestion()
         {
-            //CurrentQuiz.Add(new Model.Question(Answers,))
-            //MessageBox.Show(Question+ Answers[0]+ Answers[1]);
+            CurrentQuiz?.Add(new Model.Question(Answers, Checked, Question));
+            MessageBox.Show(Question + Answers[0] + Checked[0] + Answers[1] + Checked[1]);
+            Clear();
+        }
+        private void Clear()
+        {
+            for(int i = 0; i < Answers.Length; i++)
+            {
+                Answers[i] = string.Empty;
+            }
+            for(int i = 0; i < Checked.Length; i++)
+            {
+                Checked[i] = false;
+            }
+            Question = string.Empty;
+            OnPropertyChanged(nameof (Question));
+            OnPropertyChanged(nameof (Answers));
+            OnPropertyChanged(nameof (Checked));
         }
     }
 }
